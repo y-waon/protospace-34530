fbcc4db8ee814cb5c4195ec9c9dd7a2bba7a048f
y-waon <you0923fs@gmail.com>
2021-02-16 17:39:01 +0900

    Update prototypes_controller.rb

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :destoroy]
+  before_action :authenticate_user!, only: [:new, :edit, :destoroy]
   before_action :move_to_index, except: [:index, :show]
 
   def index
